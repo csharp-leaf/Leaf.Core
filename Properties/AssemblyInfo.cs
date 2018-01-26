2cdb622990550c6d420c6b5271c8a1d71b63053d
grandsilence <mixtape774@gmail.com>
2018-01-26 21:15:13 +0300

    Update version.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("Leaf.Core")]
-[assembly: AssemblyDescription("https://github.com/grandsilence/Leaf.Core")]
+[assembly: AssemblyDescription("https://github.com/csharp-leaf/Leaf.Core")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kelog Studio")]
 [assembly: AssemblyProduct("Leaf.Core")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
