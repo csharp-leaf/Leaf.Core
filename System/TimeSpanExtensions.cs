faeebb0c49c998e47fb9c7b81e413e0273e89ad2
grandsilence <mixtape774@gmail.com>
2018-03-10 00:36:18 +0300

    TimeSpan ToPrettyString() for pretty print

---
+++ System/TimeSpanExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Leaf.Core.System
+{
+    public static class TimeSpanExtensions
+    {
+        public static string ToPrettyString(this TimeSpan self)
+        {
+            var sb = new StringBuilder();
+
+            if (self.Days > 0)
+            {
+                sb.Append(self.Days);
+                sb.Append(" дней ");
+            }
+            sb.AppendFormat(CultureInfo.InvariantCulture,
+                "{0:00}:{1:00}:{2:00}", self.Hours, self.Minutes, self.Seconds);
+
+            return sb.ToString();
+        }
+    }
+}
