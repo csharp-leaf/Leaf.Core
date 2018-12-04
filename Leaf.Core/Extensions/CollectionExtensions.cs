07ddb5537471632806e23cbd8c56f60908b30b98
grandsilence <mixtape774@gmail.com>
2018-12-04 16:19:29 +0300

    Added Randomizer and extension for all collections "NextRandom".

---
+++ Leaf.Core/Extensions/CollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Leaf.Core.Extensions
+{
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Получить следующий случайный элемент.
+        /// </summary>
+        /// <typeparam name="T">Тип элементов в коллекции</typeparam>
+        /// <param name="list">Список с элементами</param>
+        /// <returns>Вернет случайный элемент из коллекции.</returns>
+        public static T NextRandom<T>(this IReadOnlyList<T> list)
+        {
+            if (list.Equals(default(IReadOnlyList<T>)) || list.Count == 0)
+                return default(T);
+
+            return list[Randomizer.Instance.Next(0, list.Count)];
+        }
+    }
+}
---
+++ Leaf.Core/Randomizer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Leaf.Core
+{
+    /// <summary>
+    /// Singleton for Random class.
+    /// </summary>
+    public static class Randomizer
+    {
+        [ThreadStatic] private static Random _rand;
+        public static Random Instance => _rand ?? (_rand = new Random());
+    }
+}
